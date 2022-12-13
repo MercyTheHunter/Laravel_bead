fc09d5615c23742aa69e78b294681f564e071068
Mihály <mihalyvass084@gmail.com>
2022-12-13 16:58:51 +0100

    vm-1213-1

--- routes/auth.php
+++ routes/auth.php
@@ -12,8 +12,9 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    /*
     Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+                ->name('register');*/
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
@@ -21,9 +22,9 @@ Route::middleware('guest')->group(function () {
                 ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
+    /*
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
+                ->name('password.request');*/
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
                 ->name('password.email');
@@ -36,12 +37,14 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    /*
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->name('verification.notice');
+                ->name('verification.notice');*/
 
+    /*
     Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
                 ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
+                ->name('verification.verify');*/
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware('throttle:6,1')
