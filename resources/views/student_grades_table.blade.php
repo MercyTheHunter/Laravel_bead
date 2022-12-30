f06273bc8c141877755934b45692887993c688af
MercyTheHunter <vargajeff21@gmail.com>
2022-12-30 20:54:44 +0100

    rk-1230-7

--- resources/views/student_grades_table.blade.php
+++ resources/views/student_grades_table.blade.php
@@ -8,9 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Jegyek") }}
-                </div>
                 <div class="flex flex-col">
                     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
